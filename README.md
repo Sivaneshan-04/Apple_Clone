b9411c639a0445761cda9f42dc62ff474f8e8fba
Sivaneshan-04 <ed21b062@smail.iitm.ac.in>
2024-06-03 23:03:31 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# About the Project 
+This is Apple Iphone 15 Pro Website build with ReactJs using Three.Js and GSAP for animations. This is a complete responsive website which can be used in both Desktop and mobile devices. 
+
+# Steps to run the project
+1. Clone this repository to your local device
+
+2. Run cd APPLE_WEBSITE_CLONE
+
+3. Run npm install to install necessary dependancies
+
+4. Run npm run dev to start the project in your local environment
+
+# References
+To completely build this website from Scratch, you can follow along this youtube video https://www.youtube.com/watch?v=kRQbRAJ4-Fs by JavaScript Mastery.
+
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://6c156a38ce36857934da75f0267d738a@o4507327763906560.ingest.us.sentry.io/4507327766724608",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration(
+      {
+        useEffect:React.useEffect,
+      }
+    ),
+    Sentry.replayIntegration({
+      maskAllText:false,
+      blockAllMedia: false
+    }),
+
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
